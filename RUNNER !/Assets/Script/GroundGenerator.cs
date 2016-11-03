2f5a36e5dd7012c739d2b65c39c2fa0bc98a4ae8
Thoby <gabriel.tobby@yahoo.co.id>
2016-11-03 22:23:47 +0700

    Tugas adbo start

---
+++ RUNNER !/Assets/Script/GroundGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGenerator : MonoBehaviour {
+    public GameObject theGround;
+    public Transform generationPoint;
+    private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+        platformWidth = theGround.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < generationPoint.position.x)
+        {
+            transform.position = new Vector3(transform.position.x + platformWidth, transform.position.y, transform.position.z);
+            Instantiate(theGround, transform.position, transform.rotation);
+        }
+	}
+}
---
+++ RUNNER !/Assets/Script/movingPlayer.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movingPlayer : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public AudioSource jumpSound;
+    public bool grounded = true;
+    public LayerMask whatIsGround;
+
+    private Rigidbody2D myRigidBody;
+    private Collider2D myCollider;
+    private Animator myAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+        myCollider = GetComponent<Collider2D>();
+
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+        myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            if (grounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                jumpSound.Play();
+                myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+            }
+        }
+        myAnimator.SetFloat("speed", myRigidBody.velocity.x);
+        myAnimator.SetBool("grounded", grounded);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "New Sprite")
+        {
+            Debug.Log("TRIGGERED!");
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.name == "New Sprite")
+        {
+            Debug.Log("COLLIDED!");
+        }
+    }
+}
