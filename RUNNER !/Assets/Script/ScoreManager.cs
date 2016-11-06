fff413d572c0c5af48e0eca84fb3c8774ab38a2f
billyAdi <billz.wew@gmail.com>
2016-11-06 20:29:38 +0700

    Tambahan Main menu dan score

---
+++ RUNNER !/Assets/Script/ScoreManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+	public Text scoreText;
+	public Text hiScoreText;
+
+	public float scoreCount;
+	public float hiScoreCount;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreCount += 5 * Time.deltaTime;
+		if (scoreCount > hiScoreCount) {
+			hiScoreCount = scoreCount;
+		}
+		scoreText.text = "Score: " + Mathf.Round (scoreCount);
+		hiScoreText.text = "High Score: " +  Mathf.Round(hiScoreCount);
+	}
+}
