30deea1ce91f9752819344dae296b0c14babae23
billyAdi <billz.wew@gmail.com>
2016-11-06 19:29:24 +0700

    Add files via upload

---
+++ RUNNER%20!/Assets/Script/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+	
+	public void PlayGame(){
+		SceneManager.LoadScene ("endlessRun5");
+			}
+
+	public void QuitGame(){
+
+		Application.Quit ();
+	}
+}
