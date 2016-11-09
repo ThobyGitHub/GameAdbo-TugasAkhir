7a353970075f29c4c2687b4089572c8e1ab23413
Jonathan Alva <exzone97@yahoo.co.id>
2016-11-09 14:25:01 +0700

    Background Scrolling

---
+++ RUNNER !/Assets/Script/BackGroundScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGroundScript : MonoBehaviour {
+	public float backgroundSize;
+
+	private Transform cameraTransform;
+	private Transform[] layers;
+	private float viewZone = 10;
+	private int leftIdx;
+	private int rightIdx;
+
+	public float paralaxSpeed;
+	private float lastCameraX;
+
+	private void Start(){
+		cameraTransform = Camera.main.transform;
+		lastCameraX = cameraTransform.position.x;
+
+		layers = new Transform[transform.childCount];
+		for (int i = 0; i < transform.childCount; i++) 
+			layers [i] = transform.GetChild (i);
+
+		leftIdx = 0;
+		rightIdx = layers.Length - 1;
+
+	}
+
+	public void ScrollRight(){
+		int lastLeft = leftIdx;
+		layers [leftIdx].position = Vector3.right*(layers [rightIdx].position.x + backgroundSize);
+		rightIdx = leftIdx;
+		leftIdx++;
+		if (leftIdx == layers.Length)
+			leftIdx = 0;
+	}
+
+	public void ScrollLeft(){
+		int lastRight = rightIdx;
+		layers [rightIdx].position = Vector3.right*(layers [leftIdx].position.x - backgroundSize);
+		leftIdx = rightIdx;
+		rightIdx--;
+		if (rightIdx < 0)
+			rightIdx = layers.Length;
+	}
+	private void Update(){
+		float deltaX = cameraTransform.position.x - lastCameraX;
+		transform.position+= Vector3.right * (deltaX * paralaxSpeed);
+		lastCameraX = cameraTransform.position.x;
+
+
+		if (cameraTransform.position.x < (layers [leftIdx].transform.position.x - viewZone))
+			ScrollLeft ();
+		if (cameraTransform.position.x > (layers [rightIdx].transform.position.x - viewZone))
+			ScrollRight ();
+	}
+
+}
