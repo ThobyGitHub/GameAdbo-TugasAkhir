0b295f84d2d2510a2ff4f4839aec4393fa88393a
JoshuaRiyadi <riyadijoshua@gmail.com>
2016-11-09 13:20:29 +0700

    obstacle

---
+++ RUNNER !/Assets/Script/Obstacle.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+    public GameObject[] obstacle;
+    public Transform cam;
+	// Use this for initialization
+	void Start () {
+        ObstacleMaker();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * PlayerPrefs.GetInt("Speed") * Time.deltaTime);
+    }
+
+    void ObstacleMaker()
+    {
+        GameObject clone = (GameObject)Instantiate(obstacle[Random.Range(0, obstacle.Length)], transform.position, Quaternion.identity);
+        clone.name = "Quad";
+        clone.AddComponent<BoxCollider2D>();
+        clone.GetComponent<BoxCollider2D>().isTrigger = true;
+        float xx = Random.Range(1, 5);
+        Invoke("ObstacleMaker", xx);
+    }
+}
