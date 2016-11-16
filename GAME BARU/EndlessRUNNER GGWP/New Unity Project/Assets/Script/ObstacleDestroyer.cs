2906083057e5d6f14bce06196a8979ade9739cb3
billyAdi <billz.wew@gmail.com>
2016-11-16 22:00:04 +0700

    Commit Ulang
    Yang sebelumnya salah commit

---
+++ GAME BARU/EndlessRUNNER GGWP/New Unity Project/Assets/Script/ObstacleDestroyer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleDestroyer : MonoBehaviour {
+
+	public GameObject platformDestructionPoint;
+
+	// Use this for initialization
+	void Start () {
+		platformDestructionPoint = GameObject.Find ("ObstacleDestructionPoint");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < platformDestructionPoint.transform.position.x) {
+			Destroy (gameObject);
+			gameObject.SetActive(false);
+		}
+	}
+}
