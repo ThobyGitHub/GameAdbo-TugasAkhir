72150849fa85b9c3cb99c15d976c33df41794c50
Jonathan Alva <exzone97@yahoo.co.id>
2016-11-16 12:39:44 +0700

    New Background Parallax Added

---
+++ GAME BARU/EndlessRUNNER GGWP/New Unity Project/Assets/Script/BackgroundScrollScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundScrollScript : MonoBehaviour {
+
+	float offset;
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		offset = Time.time * 0.2f * speed;
+		GetComponent<Renderer> ().material.SetTextureOffset ("_MainTex", new Vector2 (offset, 0));
+	}
+}
