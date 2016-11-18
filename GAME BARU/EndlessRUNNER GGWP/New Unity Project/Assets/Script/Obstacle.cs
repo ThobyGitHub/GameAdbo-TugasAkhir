c5b75e593a71ff258208e518fbd6f663dec9ff42
billyAdi <billz.wew@gmail.com>
2016-11-18 12:12:19 +0700

    edited

--- GAME BARU/EndlessRUNNER GGWP/New Unity Project/Assets/Script/Obstacle.cs
+++ GAME BARU/EndlessRUNNER GGWP/New Unity Project/Assets/Script/Obstacle.cs
@@ -8,13 +8,16 @@ public class Obstacle : MonoBehaviour {
 
     private int idx;
     private Vector2 posNow;
+	private float posY;
     void Start () {
         posNow = GameObject.Find("Obstacle").transform.position;
+		posY = 1.25f;
     }
 	
 	// Update is called once per frame
 	void Update () {
-        posNow = new Vector2(GameObject.Find("Player").transform.position.x, posNow.y);
+        //posNow = new Vector2(GameObject.Find("Player").transform.position.x, posY);
+		posNow = new Vector2(GameObject.Find("Player").transform.position.x, transform.position.y);
     }
 
     void OnTriggerEnter2D(Collider2D other) {
@@ -30,12 +33,14 @@ public class Obstacle : MonoBehaviour {
         idx = Random.Range(0, obstacle.Length);
         if (idx == 1)
         {
-            posNow = new Vector2(posNow.x + Random.Range(20, 30), posNow.y);
+			//posNow = new Vector2(posNow.x + Random.Range(20, 30), posY+Random.Range(-0.53f, 0.57f));
+			posNow = new Vector2(posNow.x + Random.Range(20, 30),transform.position.y );
            Instantiate(obstacle[idx], posNow, Quaternion.identity);
         }
         else
         {
-            posNow = new Vector2(posNow.x + Random.Range(20, 30), posNow.y);
+			//posNow = new Vector2(posNow.x + Random.Range(20, 30), posY+Random.Range(-0.53f, 0.57f));
+			posNow = new Vector2(posNow.x + Random.Range(20, 30),transform.position.y );
             Instantiate(obstacle[idx], posNow, Quaternion.identity);
         }
         //posNow += Vector2.right * Random.Range(10, 20);
